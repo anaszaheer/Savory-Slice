b070d6d5e3affce03e8e9a6bdb6994d18235e5ea
Anas Zaheer <anaszteen@gmail.com>
2024-06-07 23:59:09 +0500

    uploads

--- api/index.js
+++ api/index.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const exphbs = require('express-handlebars')
+const express = require("express");
+const app = express();
+const port = 3000;
+const exphbs = require("express-handlebars");
+const path = require("path");
 
-app.engine('handlebars', exphbs.engine())
-app.set('view engine', 'handlebars')
-app.set('views', path.join(__dirname, '../views'));
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "../views"));
 
 //static folder
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //routes
-const main = require('../routes/main')
+const main = require("../routes/main");
 
-app.use('/', main)
+app.use("/", main);
 
-module.exports = app;
-// app.listen(port, console.log(`Listening on port: ${port}`))
+// module.exports = app;
+app.listen(port, console.log(`Listening on port: ${port}`))
