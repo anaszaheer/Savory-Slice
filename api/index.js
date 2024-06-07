5d55c13cca3e92c00efc29b36faccd6d99e6f579
Anas Zaheer <anaszteen@gmail.com>
2024-06-08 00:08:09 +0500

    uploads

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "../views"));
 
 //static folder
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
 
 //routes
 const main = require("../routes/main");
